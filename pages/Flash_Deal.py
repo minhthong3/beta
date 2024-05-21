13c72fee253d4d901aec9d60d641428f8b8c34cd
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-05-21 22:10:37 +0700

    Update Flash_Deal.py

--- pages/Flash_Deal.py
+++ pages/Flash_Deal.py
@@ -1,36 +1,22 @@
 import streamlit as st
 
 def main():
-    st.title("Nhúng đoạn mã từ Canva vào ứng dụng Streamlit")
+    st.title("Nhúng Canva vào ứng dụng Streamlit")
 
-    # Đoạn mã HTML và CSS từ Canva
-    canva_code = '''
-    <style>
-        .full-screen-container {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            overflow: hidden;
-            z-index: 9999;
-        }
-
-        .canva-iframe {
-            width: 100%;
-            height: 100%;
-            border: none;
-        }
-    </style>
-
-    <div class="full-screen-container">
-        <iframe class="canva-iframe" src="https://www.canva.com/design/DAGF3tP2xAo/6WUfuT0272AxPgYsV9593w/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+    # Đoạn mã HTML từ Canva
+    canva_html = '''
+    <div style="position: relative; width: 100%; height: 0; padding-top: 56.2225%;
+        padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+        border-radius: 8px; will-change: transform;">
+        <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+            src="https://www.canva.com/design/DAGF3tP2xAo/6WUfuT0272AxPgYsV9593w/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
         </iframe>
     </div>
+    <a href="https://www.canva.com/design/DAGF3tP2xAo/6WUfuT0272AxPgYsV9593w/view?utm_content=DAGF3tP2xAo&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Design</a> by Minh Thông Nguyễn
     '''
 
-    # Hiển thị đoạn mã bằng cách sử dụng st.components.html
-    st.components.v1.html(canva_code)
+    # Hiển thị đoạn mã HTML bằng cách sử dụng st.markdown với option unsafe_allow_html=True
+    st.markdown(canva_html, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
