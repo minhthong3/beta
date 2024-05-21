b4237beb0e6ce28e0e29a2e9228e745cd6d41b39
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-05-21 21:16:32 +0700

    Update Dau_tu_Danh_muc.py

--- pages/Dau_tu_Danh_muc.py
+++ pages/Dau_tu_Danh_muc.py
@@ -127,3 +127,20 @@ for card in cards:
 
 # Close container for cards
 st.markdown('</div>', unsafe_allow_html=True)
+
+
+import streamlit as st
+
+# HTML code to embed
+html_code = '''
+<div style="position: relative; width: 100%; height: 0; padding-top: 141.4286%; padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden; border-radius: 8px; will-change: transform;">
+  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0; margin: 0;"
+    src="https://www.canva.com/design/DAGF3gLOxfY/r7jI3obh1jMio2XWo5OOHw/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div>
+<a href="https://www.canva.com/design/DAGF3gLOxfY/r7jI3obh1jMio2XWo5OOHw/view?utm_content=DAGF3gLOxfY&utm_campaign=designshare&utm_medium=embeds&utm_source=link" target="_blank" rel="noopener">Green White Illustrative Biology photosynthetic organisms Poster</a> by Minh Thông Nguyễn
+'''
+
+# Display the HTML in Streamlit
+st.markdown(html_code, unsafe_allow_html=True)
+
