271474d5eed295fd5138eff5f33fbcf4ebd1bb0f
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-05-21 22:12:36 +0700

    Update Flash_Deal.py

--- pages/Flash_Deal.py
+++ pages/Flash_Deal.py
@@ -10,15 +10,36 @@ def main():
             width: 100%;
             max-width: 100%;
             padding: 0;
+            margin: 0;
+        }
+
+        .canva-container {
+            position: relative;
+            width: 100%;
+            height: 0;
+            padding-top: 56.2225%;
+            margin-top: 1.6em;
+            margin-bottom: 0.9em;
+            overflow: hidden;
+            border-radius: 8px;
+            box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16);
+        }
+
+        .canva-iframe {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            border: none;
+            padding: 0;
+            margin: 0;
         }
     </style>
 
     <div class="full-width-content">
-        <div style="position: relative; width: 100%; height: 0; padding-top: 56.2225%;
-            padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
-            border-radius: 8px; will-change: transform;">
-            <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
-                src="https://www.canva.com/design/DAGF3tP2xAo/6WUfuT0272AxPgYsV9593w/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+        <div class="canva-container">
+            <iframe class="canva-iframe" src="https://www.canva.com/design/DAGF3tP2xAo/6WUfuT0272AxPgYsV9593w/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
             </iframe>
         </div>
         <a href="https://www.canva.com/design/DAGF3tP2xAo/6WUfuT0272AxPgYsV9593w/view?utm_content=DAGF3tP2xAo&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Design</a> by Minh Thông Nguyễn
