7728c04de4748a1aa2fe0f59cadd829948de1595
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-05-21 13:27:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,21 @@
 import streamlit as st
 
+# Link to the respective pages
+if selected_item == "Thông tin thị trường":
+    st.experimental_rerun("Thong_tin_thi_truong")
+elif selected_item == "Cổ phiếu chọn lọc":
+    st.experimental_rerun("Co_phieu_chon_loc")
+elif selected_item == "Đầu tư Danh mục":
+    st.experimental_rerun("Dau_tu_Danh_muc")
+elif selected_item == "Flash Deal":
+    st.experimental_rerun("Flash_Deal")
+elif selected_item == "Hướng dẫn sử dụng":
+    st.experimental_rerun("Huong_dan_su_dung")
+elif selected_item == "Liên hệ":
+    st.experimental_rerun("Lien_he")
+
+# Additional main page content
+st.write("Chào mừng bạn đến với trang web của chúng tôi! Tại đây, bạn có thể tìm hiểu thêm về thị trường, các cổ phiếu chọn lọc, cách đầu tư danh mục, và các Flash Deal hiện tại. Hãy liên hệ với chúng tôi nếu bạn cần hỗ trợ hoặc hướng dẫn sử dụng.")
 # Sidebar title
 st.sidebar.title("VNWEALTH")
 
@@ -18,20 +34,3 @@ selected_item = st.sidebar.radio(
     list(menu_items.keys()),
     format_func=lambda x: f"{menu_items[x]} {x}"
 )
-
-# Link to the respective pages
-if selected_item == "Thông tin thị trường":
-    st.experimental_rerun("Thong_tin_thi_truong")
-elif selected_item == "Cổ phiếu chọn lọc":
-    st.experimental_rerun("Co_phieu_chon_loc")
-elif selected_item == "Đầu tư Danh mục":
-    st.experimental_rerun("Dau_tu_Danh_muc")
-elif selected_item == "Flash Deal":
-    st.experimental_rerun("Flash_Deal")
-elif selected_item == "Hướng dẫn sử dụng":
-    st.experimental_rerun("Huong_dan_su_dung")
-elif selected_item == "Liên hệ":
-    st.experimental_rerun("Lien_he")
-
-# Additional main page content
-st.write("Chào mừng bạn đến với trang web của chúng tôi! Tại đây, bạn có thể tìm hiểu thêm về thị trường, các cổ phiếu chọn lọc, cách đầu tư danh mục, và các Flash Deal hiện tại. Hãy liên hệ với chúng tôi nếu bạn cần hỗ trợ hoặc hướng dẫn sử dụng.")
