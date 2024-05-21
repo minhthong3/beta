a542faa8b196b223c38626eab8f9e05b27d2c07b
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-05-21 21:26:57 +0700

    Update Dau_tu_Danh_muc.py

--- pages/Dau_tu_Danh_muc.py
+++ pages/Dau_tu_Danh_muc.py
@@ -3,6 +3,17 @@ from PIL import Image
 import base64
 from io import BytesIO
 
+# CSS to make the main content full width
+css_code = """
+<style>
+    .main > div {
+        max-width: 100%;
+        padding-left: 5%;
+        padding-right: 5%;
+    }
+</style>
+"""
+
 # Function to convert image to base64
 @st.cache_data
 def image_to_base64(img_path, resize_to=(300, 300), quality=85):
@@ -19,14 +30,9 @@ image_3_base64 = image_to_base64("image.danhmuc/image_3.png")
 image_4_base64 = image_to_base64("image.danhmuc/image_4.png")
 image_5_base64 = image_to_base64("image.danhmuc/image_5.png")
 
-# Custom CSS to style the cards and make the main content full width
+# Custom CSS to style the cards
 st.markdown("""
 <style>
-.main > div {
-    max-width: 100%;
-    padding-left: 5%;
-    padding-right: 5%;
-}
 .card-container {
     display: flex;
     justify-content: space-between;
@@ -132,19 +138,6 @@ for col, card in zip(cols, cards):
         </div>
         """, unsafe_allow_html=True)
 
-
-# HTML code to embed
-html_code = '''
-<div style="position: relative; width: 100%; height: 0; padding-top: 141.4286%; padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden; border-radius: 8px; will-change: transform;">
-  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0; margin: 0;"
-    src="https://www.canva.com/design/DAGF3gLOxfY/r7jI3obh1jMio2XWo5OOHw/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
-  </iframe>
-</div>
-<a href="https://www.canva.com/design/DAGF3gLOxfY/r7jI3obh1jMio2XWo5OOHw/view?utm_content=DAGF3gLOxfY&utm_campaign=designshare&utm_medium=embeds&utm_source=link" target="_blank" rel="noopener">Green White Illustrative Biology photosynthetic organisms Poster</a> by Minh Thông Nguyễn
-'''
-
-# Apply the custom CSS
-st.markdown(css_code, unsafe_allow_html=True)
 
 # Display the HTML in Streamlit
 st.markdown(html_code, unsafe_allow_html=True)
