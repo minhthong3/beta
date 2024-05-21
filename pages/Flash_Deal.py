e188cafaee813093637a9fe9960f13bb7f011ac5
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-05-21 22:05:58 +0700

    Update Flash_Deal.py

--- pages/Flash_Deal.py
+++ pages/Flash_Deal.py
@@ -1,20 +1,22 @@
 import streamlit as st
 
-st.title("Flash Deal")
-st.header("Flash Deal")
-st.write("Nội dung cho phần Flash Deal...")
-
-import streamlit as st
-
 def main():
-    st.title("Nhúng Canva vào ứng dụng Streamlit")
+    st.title("Nhúng đoạn mã từ Canva vào ứng dụng Streamlit")
 
-    # Đường dẫn đến trang web Canva cụ thể
-    canva_url = "https://www.canva.com/design/DAGF3tP2xAo/Js0hl4gPnM9QgnWkpPL2Ng/view?utm_content=DAGF3tP2xAo&utm_campaign=designshare&utm_medium=link&utm_source=editor"
+    # Đoạn mã từ Canva
+    canva_code = '''
+    <div style="position: relative; width: 100%; height: 0; padding-top: 56.2225%;
+    padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+    border-radius: 8px; will-change: transform;">
+    <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+        src="https://www.canva.com/design/DAGF3tP2xAo/6WUfuT0272AxPgYsV9593w/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+    </iframe>
+    </div>
+    <a href="https://www.canva.com/design/DAGF3tP2xAo/6WUfuT0272AxPgYsV9593w/view?utm_content=DAGF3tP2xAo&utm_campaign=designshare&utm_medium=embeds&utm_source=link" target="_blank" rel="noopener">Design</a>
+    '''
 
-    # Hiển thị trang web Canva bằng iframe
-    st.write(f'<iframe src="{canva_url}" width="800" height="600"></iframe>', unsafe_allow_html=True)
+    # Hiển thị đoạn mã bằng cách sử dụng st.markdown với option unsafe_allow_html=True
+    st.markdown(canva_code, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
-
