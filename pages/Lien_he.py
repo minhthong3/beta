65cc0a78c5f1563ac72b0e38d2c247935f473d67
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-05-21 22:31:12 +0700

    Update Lien_he.py

--- pages/Lien_he.py
+++ pages/Lien_he.py
@@ -3,3 +3,15 @@ import streamlit as st
 st.title("Liên hệ")
 st.header("Liên hệ")
 st.write("Nội dung cho phần Liên hệ...")
+
+
+
+# Đường link Canva cần nhúng
+canva_link = "https://aaaadfe.my.canva.site/"
+
+# Mã HTML để nhúng liên kết Canva
+html_code = f'<iframe src="{canva_link}" width="100%" height="600px" frameborder="0" allowfullscreen></iframe>'
+
+# Hiển thị HTML trong Streamlit
+st.markdown(html_code, unsafe_allow_html=True)
+
