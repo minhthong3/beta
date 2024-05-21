4d8f24888daa2b01ff97a220e0b7917b426807d9
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-05-21 21:59:29 +0700

    Update Flash_Deal.py

--- pages/Flash_Deal.py
+++ pages/Flash_Deal.py
@@ -10,7 +10,7 @@ def main():
     st.title("Nhúng Canva vào ứng dụng Streamlit")
 
     # Đường dẫn đến trang web Canva cụ thể
-    canva_url = "https://www.canva.com/design/DAGF3tP2xAo/6-xQ5Yb_tmASLNd2leHL_g/edit?utm_content=DAGF3tP2xAo&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+    canva_url = "https://www.canva.com/design/DAGF3tP2xAo/Js0hl4gPnM9QgnWkpPL2Ng/view?utm_content=DAGF3tP2xAo&utm_campaign=designshare&utm_medium=link&utm_source=editor"
 
     # Hiển thị trang web Canva bằng iframe
     st.write(f'<iframe src="{canva_url}" width="800" height="600"></iframe>', unsafe_allow_html=True)
