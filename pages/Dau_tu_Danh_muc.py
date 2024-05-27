c26d3b9ca6c6066d44f9eb34dddcd68675f0a908
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-05-27 21:17:57 +0700

    Update Dau_tu_Danh_muc.py

--- pages/Dau_tu_Danh_muc.py
+++ pages/Dau_tu_Danh_muc.py
@@ -46,6 +46,17 @@ tab1, tab2, tab3, tab4, tab5 = st.tabs(["Đầu tư giá trị", "Đầu tư tă
 with tab1:
     st.header("Đầu tư giá trị")
     st.write("Đây là nội dung của Tab 1.")
+    # Mã HTML để nhúng
+    html_code = '''
+    <div style="position: relative; width: 100%; height: 0; padding-top: 56.2225%;
+     padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+     border-radius: 8px; will-change: transform;">
+      <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+        src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGGbeRekAE&#x2F;O1EpPEZ1Of7YCziHtJCm-g&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+          </iframe>
+    </div>
+    <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGGbeRekAE&#x2F;O1EpPEZ1Of7YCziHtJCm-g&#x2F;view?utm_content=DAGGbeRekAE&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">
+    '''
 
 # Nội dung cho tab 2
 with tab2:
