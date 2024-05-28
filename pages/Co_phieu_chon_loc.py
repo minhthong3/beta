f66db3b3226b9e12f7c8d066cd89ba086fe38d72
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-05-28 10:45:56 +0700

    Update Co_phieu_chon_loc.py

--- pages/Co_phieu_chon_loc.py
+++ pages/Co_phieu_chon_loc.py
@@ -1,18 +1,5 @@
 import streamlit as st
 import pandas as pd
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-
-# Thiết lập thông tin đăng nhập và quyền truy cập
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("image.danhmuc/datavnwealth-25a353ea3781.json", scope)
-client = gspread.authorize(creds)
-
-# Lấy dữ liệu từ Google Sheets
-spreadsheet = client.open_by_url("https://docs.google.com/spreadsheets/d/19TRaGNqO9darTuF5_2xCLEqoWOXZpUiTIIQMP1JeiTU/edit?usp=sharing")
-sheet = spreadsheet.sheet1
-data = sheet.get_all_records()
-df = pd.DataFrame(data)
 
 # CSS tùy chỉnh cho bảng
 css = """
@@ -34,31 +21,29 @@ css = """
         background-color: #4CAF50;
         color: white;
     }
-    .highlight {
-        background-color: lightgreen !important;
-    }
 </style>
 """
-
-# Hàm để áp dụng định dạng có điều kiện
-def highlight_mua(val):
-    color = 'lightgreen' if val == "Mua" else ''
-    return f'background-color: {color}'
-
-# Áp dụng định dạng có điều kiện cho cột "Khuyến nghị"
-if 'Khuyến nghị' in df.columns:
-    df = df.style.applymap(highlight_mua, subset=['Khuyến nghị'])
 
 # Tiêu đề của ứng dụng
 st.title("Hiển thị Bảng Dữ liệu từ Google Sheets")
 
-# Áp dụng CSS tùy chỉnh
-st.markdown(css, unsafe_allow_html=True)
+# URL CSV từ Google Sheets
+google_sheet_csv_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTn8rFdZh3D0fL9_k1VpGflP8OeJlZoEE5tA4ISUwx3u8SdsbJZt9ohSzkEJSI1pxy5Z1iVZjFN5QQZ/pub?output=csv"
 
-# Hiển thị bảng dữ liệu với định dạng HTML
-st.write("Dữ liệu của bạn:")
-st.components.v1.html(df.to_html(classes='dataframe'), height=600, scrolling=True)
+try:
+    # Đọc tệp CSV từ Google Sheets
+    df = pd.read_csv(google_sheet_csv_url)
 
-# Hiển thị thống kê mô tả với định dạng HTML
-st.write("Thống kê mô tả:")
-st.components.v1.html(df.data.describe().to_html(classes='dataframe'), height=600, scrolling=True)
+    # Áp dụng CSS tùy chỉnh
+    st.markdown(css, unsafe_allow_html=True)
+    
+    # Hiển thị bảng dữ liệu với định dạng HTML
+    st.write("Dữ liệu của bạn:")
+    st.write(df.to_html(classes='dataframe'), unsafe_allow_html=True)
+
+    # Hiển thị thống kê mô tả với định dạng HTML
+    st.write("Thống kê mô tả:")
+    st.write(df.describe().to_html(classes='dataframe'), unsafe_allow_html=True)
+
+except Exception as e:
+    st.error(f"Không thể tải dữ liệu từ URL. Lỗi: {e}")
