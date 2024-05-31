f3a5c8c83883e44a145cb4529f29dcfa103429bb
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-05-31 12:38:04 +0700

    Update Thị_trường.py

--- pages/Thị_trường.py
+++ pages/Thị_trường.py
@@ -5,15 +5,20 @@ from streamlit_navigation_bar import st_navbar
 st.set_page_config(page_title="GoodStock Analysis", initial_sidebar_state="collapsed")
 
 # Define the pages and custom styles for the navigation bar
-pages = ["Home", "Co_phieu_chon_loc", "Examples", "Community", "About"]
+pages = ["Home", "Documentation", "Examples", "Community", "About"]
 styles = {
     "nav": {"background-color": "rgb(123, 209, 146)"},
-    "div": {"max-width": "32rem"},
+    "div": {
+        "max-width": "100%",  # Ensure the div takes up all the available width on smaller screens
+        "margin": "auto"  # Center the content
+    },
     "span": {
         "border-radius": "0.5rem",
         "color": "rgb(49, 51, 63)",
         "margin": "0 0.125rem",
         "padding": "0.4375rem 0.625rem",
+        "display": "inline-block",  # Ensure that text aligns well on smaller screens
+        "white-space": "nowrap"
     },
     "active": {"background-color": "rgba(255, 255, 255, 0.25)"},
     "hover": {"background-color": "rgba(255, 255, 255, 0.35)"},
@@ -27,8 +32,8 @@ def home():
     st.title("Home")
     st.write("Welcome to the home page!")
 
-def Co_phieu_chon_loc():
-    st.title("Co_phieu_chon_loc")
+def documentation():
+    st.title("Documentation")
     st.write("Here you can find all the documentation.")
 
 def examples():
@@ -46,8 +51,8 @@ def about():
 # Display the content based on the selected page
 if selected_page == "Home":
     home()
-elif selected_page == "Co_phieu_chon_loc":
-    st.page_link("pages/Co_phieu_chon_loc.py")
+elif selected_page == "Documentation":
+    documentation()
 elif selected_page == "Examples":
     examples()
 elif selected_page == "Community":
