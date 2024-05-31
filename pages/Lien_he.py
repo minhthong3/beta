43d28907f5599e7f7711e808afad0006e85479ac
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-05-31 14:53:34 +0700

    Update Lien_he.py

--- pages/Lien_he.py
+++ pages/Lien_he.py
@@ -6,8 +6,6 @@ from st_on_hover_tabs import on_hover_tabs
 import streamlit as st
 st.set_page_config(layout="wide")
 
-st.header("Custom tab component for on-hover navigation bar")
-st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
 
 with st.sidebar:
         tabs = on_hover_tabs(tabName=['Dashboard', 'Money', 'Economy'], 
