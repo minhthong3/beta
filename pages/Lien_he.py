f64416af1bc12cd02201fcd0acbc0e252275b241
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-05-31 15:09:38 +0700

    Update Lien_he.py

--- pages/Lien_he.py
+++ pages/Lien_he.py
@@ -3,7 +3,7 @@ from streamlit_option_menu import option_menu
 
 
 
-selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings','Settings'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear', "list-task"], 
     menu_icon="cast", default_index=0, orientation="horizontal")
 selected2
