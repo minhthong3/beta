0edaacf32dab340abbf67e8119d7ec107e58cf71
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-05-31 16:34:15 +0700

    Update Huong_dan_su_dung.py

--- pages/Huong_dan_su_dung.py
+++ pages/Huong_dan_su_dung.py
@@ -3,7 +3,13 @@ from streamlit_option_menu import option_menu
 
 # 1. as sidebar menu
 with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", 'Settings'], 
-        icons=['house', 'gear'], menu_icon="cast", default_index=1)
-    selected
-
+    # 3. CSS style definitions
+    selected3 = option_menu(None, ["Home", "Upload",  "Tasks", 'Settings'], 
+        icons=['house', 'cloud-upload', "list-task", 'gear'], 
+        menu_icon="cast", default_index=0,
+        styles={
+            "container": {"padding": "0!important", "background-color": "#fafafa"},
+            "icon": {"color": "orange", "font-size": "25px"}, 
+            "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+            "nav-link-selected": {"background-color": "green"},
+        }
