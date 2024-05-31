49d3dea5505aadaee527b6a435168fa93e1434ac
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-05-31 14:52:55 +0700

    Update Lien_he.py

--- pages/Lien_he.py
+++ pages/Lien_he.py
@@ -1,17 +1,29 @@
+from st_on_hover_tabs import on_hover_tabs
 import streamlit as st
+st.set_page_config(layout="wide")
 
-st.title("Liên hệ")
-st.header("Liên hệ")
-st.write("Nội dung cho phần Liên hệ...")
+from st_on_hover_tabs import on_hover_tabs
+import streamlit as st
+st.set_page_config(layout="wide")
 
+st.header("Custom tab component for on-hover navigation bar")
+st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
 
-
-# Đường link Canva cần nhúng
-canva_link = "https://aaaadfe.my.canva.site/"
-
-# Mã HTML để nhúng liên kết Canva
-html_code = f'<iframe src="{canva_link}" width="100%" height="600px" frameborder="0" allowfullscreen></iframe>'
-
-# Hiển thị HTML trong Streamlit
-st.markdown(html_code, unsafe_allow_html=True)
-
+with st.sidebar:
+        tabs = on_hover_tabs(tabName=['Dashboard', 'Money', 'Economy'], 
+                             iconName=['dashboard', 'money', 'economy'],
+                             styles = {'navtab': {'background-color':'#111',
+                                                  'color': '#818181',
+                                                  'font-size': '18px',
+                                                  'transition': '.3s',
+                                                  'white-space': 'nowrap',
+                                                  'text-transform': 'uppercase'},
+                                       'tabOptionsStyle': {':hover :hover': {'color': 'red',
+                                                                      'cursor': 'pointer'}},
+                                       'iconStyle':{'position':'fixed',
+                                                    'left':'7.5px',
+                                                    'text-align': 'left'},
+                                       'tabStyle' : {'list-style-type': 'none',
+                                                     'margin-bottom': '30px',
+                                                     'padding-left': '30px'}},
+                             key="1")
