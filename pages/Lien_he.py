67051841829105693ba252b0d9798a0fd76d3bdc
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-06-01 12:15:22 +0700

    Update Lien_he.py

--- pages/Lien_he.py
+++ pages/Lien_he.py
@@ -1,5 +1,8 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
+from streamlit_autorefresh import st_autorefresh
+import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 # Thiết lập tiêu đề cho ứng dụng
 st.title(" vnwPortfolio ")
