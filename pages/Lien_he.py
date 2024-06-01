874d9b13a33a47cb9ccb7b717d7c7ef1796b2a2b
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-06-01 12:07:06 +0700

    Update Lien_he.py

--- pages/Lien_he.py
+++ pages/Lien_he.py
@@ -3,14 +3,17 @@ from streamlit_option_menu import option_menu
 
 
 
-selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings','Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear', "list-task"], 
+selected2 = option_menu(None, ["Home", "Upload"], 
+    icons=['house', 'cloud-upload'], 
     menu_icon="cast", default_index=0, orientation="horizontal")
 selected2
 # Thiết lập tiêu đề cho ứng dụng
 st.title(" vnwPortfolio ")
 st. write('VNWEALTH mang đến sản phẩm Danh mục đầu tư mẫu với 5 danh mục được nghiên cứu theo chiến lược của các huyền thoại đầu tư như Warren Buffett hay Philip Fisher, để sẵn sàng cùng bạn chạm được cột mốc cao nhất trong hành trình này.')
 st. write('Danh mục cổ phiếu là một tập hợp các cổ phiếu được lựa chọn từ nhiều ngành và lĩnh vực khác nhau. Mục đích của việc xây dựng danh mục này là đa dạng hóa đầu tư, giảm thiểu rủi ro và tối đa hóa lợi nhuận dài hạn')
+
+if selected2 == "Home"
+
 # CSS để làm cho nội dung chính rộng toàn màn hình
 css_code = """
 <style>
@@ -64,23 +67,7 @@ with tab1:
     </div>
     <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGGbeRekAE&#x2F;O1EpPEZ1Of7YCziHtJCm-g&#x2F;view?utm_content=DAGGbeRekAE&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">
     '''
+else:
+    st.write("settings is my bettings")
 
-# Nội dung cho tab 2
-with tab2:
-    st.header("Đầu tư tăng trưởng")
-    st.write("Đây là nội dung của Tab 2.")
 
-# Nội dung cho tab 3
-with tab3:
-    st.header("Cổ tức bền vững")
-    st.write("Đây là nội dung của Tab 3.")
-
-# Nội dung cho tab 4
-with tab4:
-    st.header("Đầu tư phòng thủ")
-    st.write("Đây là nội dung của Tab 4.")
-
-# Nội dung cho tab 5
-with tab5:
-    st.header("Cơ hội đổi đời")
-    st.write("Đây là nội dung của Tab 5.")
