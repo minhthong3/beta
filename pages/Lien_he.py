851f761aabaa8c6f947480c8c2ab03d6ff1e0841
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-06-01 12:08:35 +0700

    Update Lien_he.py

--- pages/Lien_he.py
+++ pages/Lien_he.py
@@ -12,7 +12,7 @@ st.title(" vnwPortfolio ")
 st. write('VNWEALTH mang đến sản phẩm Danh mục đầu tư mẫu với 5 danh mục được nghiên cứu theo chiến lược của các huyền thoại đầu tư như Warren Buffett hay Philip Fisher, để sẵn sàng cùng bạn chạm được cột mốc cao nhất trong hành trình này.')
 st. write('Danh mục cổ phiếu là một tập hợp các cổ phiếu được lựa chọn từ nhiều ngành và lĩnh vực khác nhau. Mục đích của việc xây dựng danh mục này là đa dạng hóa đầu tư, giảm thiểu rủi ro và tối đa hóa lợi nhuận dài hạn')
 
-if selected2 == "Home"
+if selected2 == "Home":
 
 # CSS để làm cho nội dung chính rộng toàn màn hình
 css_code = """
@@ -49,24 +49,7 @@ st.markdown(html_code, unsafe_allow_html=True)
 # Thiết lập tiêu đề cho ứng dụng
 st.title("XEM CHI TIẾT")
 st.write('Được cập nhật mỗi tháng, cập nhật gần nhất 02/05/2024')
-# Sử dụng st.tabs để tạo các tab
-tab1, tab2, tab3, tab4, tab5 = st.tabs(["Đầu tư giá trị", "Đầu tư tăng trưởng", "Cổ tức bền vững", "Đầu tư phòng thủ", "Cơ hội đổi đời"])
 
-# Nội dung cho tab 1
-with tab1:
-    st.header("Đầu tư giá trị")
-    st.write("Đây là nội dung của Tab 1.")
-    # Mã HTML để nhúng
-    html_code = '''
-    <div style="position: relative; width: 100%; height: 0; padding-top: 56.2225%;
-     padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
-     border-radius: 8px; will-change: transform;">
-      <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
-        src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGGbeRekAE&#x2F;O1EpPEZ1Of7YCziHtJCm-g&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
-          </iframe>
-    </div>
-    <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGGbeRekAE&#x2F;O1EpPEZ1Of7YCziHtJCm-g&#x2F;view?utm_content=DAGGbeRekAE&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">
-    '''
 else:
     st.write("settings is my bettings")
 
